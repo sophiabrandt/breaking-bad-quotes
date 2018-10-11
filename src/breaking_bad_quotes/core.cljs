4ec25f4106934a604d1d25ff0b83f04ef2d93de5
Sophia Brandt <git@s-br.net>
2018-10-11 20:33:23 +0200

    [update] error handling rollback - separation of concerns

--- src/breaking_bad_quotes/core.cljs
+++ src/breaking_bad_quotes/core.cljs
@@ -7,8 +7,7 @@
   (GET "https://breaking-bad-quotes.herokuapp.com/v1/quotes"
     {:handler #(reset! link %)
      :error-handler (fn [{:keys [status status-text]}]
-                        (js/console.log status status-text)
-                        (#(reset! link [{"quote" "Sorry, something went wrong."}])))}))
+                      (js/console.log status status-text))}))
 
 (defn quote []
   (let [link (atom nil)]
